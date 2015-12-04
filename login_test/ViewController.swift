ff0fd2f07d75ff6b32dfa3c9c8858bcb6f4f24dd
choheejung <choheejung@choheejungui-macbook-pro.local>
2015-12-04 22:34:22 +0900

    Initial Commit

---
+++ login_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  login_test
+//
+//  Created by choheejung on 2015. 12. 4..
+//  Copyright © 2015년 choheejung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
